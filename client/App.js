29ff36f85411fa946bca4f4f77ea930514a9732c
jesussfl <jesussflr@gmail.com>
2023-08-03 13:26:16 -0400

    Split app code and adds new bottom tab navigator

--- client/App.js
+++ client/App.js
@@ -1,72 +1,28 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useFonts } from "expo-font";
-
-import { SemanticColors } from "./src/utilities/Theme";
 import { WithSplashScreen } from "./src/screens/Splash";
+import { IntroductoryStackNavigator } from "./src/screens/navigators/IntroductoryStackNavigator";
 import * as WebBrowser from "expo-web-browser";
-import * as Screens from "./src/screens/index";
+import useCustomFonts from "./src/utilities/hooks/customFonts";
 
 WebBrowser.maybeCompleteAuthSession();
-const Stack = createNativeStackNavigator();
-
-const commonScreenOptions = {
-   gestureEnabled: true,
-   animationTypeForReplace: "pop",
-   animation: "slide_from_right",
-};
-
-const routes = [
-   { name: "Walkthrough1", component: Screens.Walkthrough1, headerShown: false },
-   { name: "Walkthrough2", component: Screens.Walkthrough2, headerShown: false },
-   { name: "Walkthrough3", component: Screens.Walkthrough3, headerShown: false },
-   { name: "Welcome", component: Screens.WelcomeScreen, headerShown: false },
-   { name: "Login", component: Screens.LoginScreen, headerShown: true, headerTitle: "" },
-   { name: "Signup", component: Screens.SignupScreen, headerShown: true, headerTitle: "" },
-   { name: "Home", component: Screens.HomeScreen, headerShown: true, headerTitle: "Inicio" },
-];
 
 export default function App() {
    const [isAppReady, setIsAppReady] = useState(false);
-   const [fontsLoaded, error] = useFonts({
-      "Sora-Bold": require("./assets/fonts/Sora-Bold.ttf"),
-      "Sora-Regular": require("./assets/fonts/Sora-Regular.ttf"),
-      "Sora-Medium": require("./assets/fonts/Sora-Medium.ttf"),
-      "Sora-Light": require("./assets/fonts/Sora-Light.ttf"),
-      "Sora-SemiBold": require("./assets/fonts/Sora-SemiBold.ttf"),
-      "Sora-ExtraLight": require("./assets/fonts/Sora-ExtraLight.ttf"),
-      "Sora-ExtraBold": require("./assets/fonts/Sora-ExtraBold.ttf"),
-   });
+   const isFontsLoaded = useCustomFonts();
 
    useEffect(() => {
-      setIsAppReady(true);
-   }, [fontsLoaded]);
+      setIsAppReady(isFontsLoaded);
+   }, [isFontsLoaded]);
 
-   if (!fontsLoaded) {
+   if (!isAppReady) {
       return null;
    }
-
-   if (error) {
-      console.error("Error loading fonts:", error);
-   }
-
    return (
       <WithSplashScreen isAppReady={isAppReady}>
          <NavigationContainer>
-            <Stack.Navigator
-               screenOptions={({ route }) => ({
-                  headerShadowVisible: false,
-                  headerStyle: { backgroundColor: SemanticColors.app.bg_normal },
-                  headerShown: route.params?.headerShown ?? commonScreenOptions.headerShown,
-                  headerTitle: route.params?.headerTitle ?? commonScreenOptions.headerTitle,
-                  ...commonScreenOptions,
-               })}>
-               {routes.map(({ name, component, headerShown, headerTitle }) => (
-                  <Stack.Screen key={name} name={name} component={component} initialParams={{ headerShown, headerTitle }} />
-               ))}
-            </Stack.Navigator>
+            <IntroductoryStackNavigator />
          </NavigationContainer>
       </WithSplashScreen>
    );
