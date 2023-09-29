9a8a6c7e74616394c19a8d47566d12a2b0062938
Jesus Lopez <jesussflr@gmail.com>
2023-09-29 09:21:45 -0400

    add production environment variables

--- api/config/env/production/database.js
+++ api/config/env/production/database.js
@@ -1,22 +1,13 @@
-// path: ./config/env/production/database.js
-
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
-      },
-      debug: false,
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5931),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
     },
-  };
-};
+  },
+});
