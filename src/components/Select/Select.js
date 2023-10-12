a7ea12fd942d975e9cede15da38eced1dc36c555
Jesus Lopez <jesussflr@gmail.com>
2023-10-11 20:34:04 -0400

    fetch images and show in simple selection exercises

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,130 +1,173 @@
-import { StyleSheet, Text, View, Image, TouchableWithoutFeedback } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 import React, { useState } from "react";
 import { SemanticColors, Colors } from "@utils/Theme";
 import RemixIcon from "react-native-remix-icon";
 
-const Select = ({ text, image, isPressed = false, error = false, success = false, onPress }) => {
-   const getStyles = () => {
-      if (error) {
-         return SelectStateStyles.error;
-      }
-      if (success) {
-         return SelectStateStyles.success;
-      }
-      if (isPressed) {
-         return SelectStateStyles.pressed;
-      }
-      return SelectStateStyles.default;
-   };
+const Select = ({
+  text,
+  image,
+  isPressed = false,
+  error = false,
+  success = false,
+  onPress,
+}) => {
+  const getStyles = () => {
+    if (error) {
+      return SelectStateStyles.error;
+    }
+    if (success) {
+      return SelectStateStyles.success;
+    }
+    if (isPressed) {
+      return SelectStateStyles.pressed;
+    }
+    return SelectStateStyles.default;
+  };
 
-   const getTextStyles = () => {
-      if (error) {
-         return TextStateStyles.error;
-      }
-      if (success) {
-         return TextStateStyles.success;
-      }
-      if (isPressed) {
-         return TextStateStyles.pressed;
-      }
-      return TextStateStyles.default;
-   };
+  const getTextStyles = () => {
+    if (error) {
+      return TextStateStyles.error;
+    }
+    if (success) {
+      return TextStateStyles.success;
+    }
+    if (isPressed) {
+      return TextStateStyles.pressed;
+    }
+    return TextStateStyles.default;
+  };
 
-   const getSelectIcon = () => {
-      if (error) {
-         return <RemixIcon name="close-circle-fill" size={24} color={Colors.error_400} />;
-      }
-      if (success) {
-         return <RemixIcon name="checkbox-circle-fill" size={24} color={Colors.success_400} />;
-      }
-      if (isPressed) {
-         return <RemixIcon name="radio-button-line" size={24} color={SemanticColors.bg.primary_normal} />;
-      }
-      return <RemixIcon name="checkbox-blank-circle-line" size={24} color={Colors.gray_300} />;
-   };
-   return (
-      <TouchableWithoutFeedback onPress={onPress}>
-         <View style={[styles.container, getStyles()]}>
-            {image ? (
-               <View style={styles.imageContainer}>
-                  <Image source={image} style={styles.image} />
-               </View>
-            ) : (
-               getSelectIcon()
-            )}
-            <Text style={[styles.text, getTextStyles()]}>{text}</Text>
-         </View>
-      </TouchableWithoutFeedback>
-   );
+  const getSelectIcon = () => {
+    if (error) {
+      return (
+        <RemixIcon
+          name="close-circle-fill"
+          size={24}
+          color={Colors.error_400}
+        />
+      );
+    }
+    if (success) {
+      return (
+        <RemixIcon
+          name="checkbox-circle-fill"
+          size={24}
+          color={Colors.success_400}
+        />
+      );
+    }
+    if (isPressed) {
+      return (
+        <RemixIcon
+          name="radio-button-line"
+          size={24}
+          color={SemanticColors.bg.primary_normal}
+        />
+      );
+    }
+    return (
+      <RemixIcon
+        name="checkbox-blank-circle-line"
+        size={24}
+        color={Colors.gray_300}
+      />
+    );
+  };
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={[styles.container, getStyles()]}>
+        {image ? (
+          <View style={styles.imageContainer}>
+            <Image
+              src={image.replace(
+                /http:\/\/localhost:1337/g,
+                process.env.EXPO_PUBLIC_API_URL
+              )}
+              style={styles.image}
+            />
+          </View>
+        ) : (
+          getSelectIcon()
+        )}
+        <Text style={[styles.text, getTextStyles()]}>{text}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 };
 
 export default Select;
 
 const SelectStateStyles = {
-   default: {
-      borderWidth: 2,
-      borderColor: Colors.gray_200,
-   },
-   pressed: {
-      borderWidth: 3,
-      borderColor: SemanticColors.bg.primary_normal,
-   },
-   success: {
-      borderWidth: 3,
-      borderColor: Colors.success_500,
-   },
-   error: {
-      borderWidth: 3,
-      borderColor: Colors.error_500,
-   },
+  default: {
+    borderWidth: 2,
+    borderColor: Colors.gray_200,
+  },
+  pressed: {
+    borderWidth: 3,
+    borderColor: SemanticColors.bg.primary_normal,
+  },
+  success: {
+    borderWidth: 3,
+    borderColor: Colors.success_500,
+  },
+  error: {
+    borderWidth: 3,
+    borderColor: Colors.error_500,
+  },
 };
 const TextStateStyles = {
-   default: {
-      fontFamily: "Sora-Regular",
-      color: Colors.gray_300,
-   },
-   pressed: {
-      fontFamily: "Sora-SemiBold",
+  default: {
+    fontFamily: "Sora-Regular",
+    color: Colors.gray_300,
+  },
+  pressed: {
+    fontFamily: "Sora-SemiBold",
 
-      color: SemanticColors.bg.primary_normal,
-   },
-   success: {
-      fontFamily: "Sora-SemiBold",
+    color: SemanticColors.bg.primary_normal,
+  },
+  success: {
+    fontFamily: "Sora-SemiBold",
 
-      color: Colors.success_500,
-   },
-   error: {
-      fontFamily: "Sora-SemiBold",
+    color: Colors.success_500,
+  },
+  error: {
+    fontFamily: "Sora-SemiBold",
 
-      color: Colors.error_500,
-   },
+    color: Colors.error_500,
+  },
 };
 const styles = StyleSheet.create({
-   container: {
-      borderRadius: 10,
-      flexDirection: "row",
-      alignItems: "center",
-      gap: 16,
-      paddingVertical: 24,
-      paddingHorizontal: 16,
-   },
+  container: {
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+    paddingVertical: 24,
+    paddingHorizontal: 16,
+  },
 
-   text: {
-      fontSize: 16,
-      flex: 1,
-      lineHeight: 24,
-   },
-   imageContainer: {
-      width: 72,
-      height: 72,
-      borderRadius: 12,
-      borderStyle: "dashed",
-      borderWidth: 2,
-      borderColor: Colors.gray_100,
-   },
-   image: {
-      width: "100%",
-      height: "100%",
-      resizeMode: "stretch",
-   },
+  text: {
+    fontSize: 16,
+    flex: 1,
+    lineHeight: 24,
+  },
+  imageContainer: {
+    width: 72,
+    height: 72,
+    borderRadius: 12,
+    borderStyle: "dashed",
+    borderWidth: 2,
+    borderColor: Colors.gray_100,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "stretch",
+  },
 });
--- src/features/Exercises/components/SimpleSelectionExercise.js
+++ src/features/Exercises/components/SimpleSelectionExercise.js
@@ -4,52 +4,73 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 import React from "react";
 import Select from "../../../components/Select/Select";
 import { Colors } from "@utils/Theme";
-const SimpleSelectionExercise = ({ content, setUserAnswer, isAnswerCorrect, userAnswer }) => {
-   return (
-      <KeyboardAwareScrollView>
-         <View style={styles.container}>
-            <Text style={{ color: Colors.gray_600, fontFamily: "Sora-SemiBold", fontSize: 20, lineHeight: 32 }}>
-               Elige la opción correcta
-            </Text>
-            <Text style={styles.text}>{content.question}</Text>
-            <View style={styles.optionsContainer}>
-               {content.options.map((option, index) => {
-                  if (userAnswer === index) {
-                     return (
-                        <Select
-                           key={index}
-                           {...option}
-                           isPressed={true}
-                           error={isAnswerCorrect === false}
-                           success={isAnswerCorrect}
-                           onPress={() => handleSelectClick(index, option.text)}></Select>
-                     );
-                  } else {
-                     return <Select key={index} {...option} isPressed={false} onPress={() => setUserAnswer(index)}></Select>;
-                  }
-               })}
-            </View>
-         </View>
-      </KeyboardAwareScrollView>
-   );
+const SimpleSelectionExercise = ({
+  content,
+  setUserAnswer,
+  isAnswerCorrect,
+  userAnswer,
+}) => {
+  console.log("content ", content);
+  return (
+    <KeyboardAwareScrollView>
+      <View style={styles.container}>
+        <Text
+          style={{
+            color: Colors.gray_600,
+            fontFamily: "Sora-SemiBold",
+            fontSize: 20,
+            lineHeight: 32,
+          }}
+        >
+          Elige la opción correcta
+        </Text>
+        <Text style={styles.text}>{content.question}</Text>
+        <View style={styles.optionsContainer}>
+          {content.options.map((option, index) => {
+            if (userAnswer === index) {
+              return (
+                <Select
+                  key={index}
+                  {...option}
+                  isPressed={true}
+                  error={isAnswerCorrect === false}
+                  success={isAnswerCorrect}
+                  onPress={() => handleSelectClick(index, option.text)}
+                ></Select>
+              );
+            } else {
+              return (
+                <Select
+                  key={index}
+                  {...option}
+                  isPressed={false}
+                  onPress={() => setUserAnswer(index)}
+                ></Select>
+              );
+            }
+          })}
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
+  );
 };
 
 export default SimpleSelectionExercise;
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      padding: 24,
-      gap: 16,
-      justifyContent: "space-between",
-   },
-   optionsContainer: {
-      gap: 24,
-   },
-   text: {
-      color: Colors.gray_500,
-      fontFamily: "Sora-Medium",
-      fontSize: 18,
-      lineHeight: 28,
-   },
+  container: {
+    flex: 1,
+    padding: 24,
+    gap: 16,
+    justifyContent: "space-between",
+  },
+  optionsContainer: {
+    gap: 24,
+  },
+  text: {
+    color: Colors.gray_500,
+    fontFamily: "Sora-Medium",
+    fontSize: 18,
+    lineHeight: 28,
+  },
 });
