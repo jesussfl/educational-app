2355c815fad1e50cc0eab847cc2456d6c62ccbc0
Jesus Lopez <jesussflr@gmail.com>
2023-10-11 21:29:50 -0400

    Improve user profile ui

--- src/features/Profile/pages/ProfileScreen.js
+++ src/features/Profile/pages/ProfileScreen.js
@@ -1,72 +1,146 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
 import { Button, TextField } from "@components";
 import { removeToken } from "@utils/helpers/auth.helpers";
 import { useNavigation } from "@react-navigation/native";
 import { useForm } from "react-hook-form";
 import Icon from "react-native-remix-icon";
+import { UserCirlceAdd } from "iconsax-react-native";
 import { Colors } from "@utils/Theme";
 import { emailValidations } from "../../Auth/utils/inputValidations";
 import { useAuthContext } from "../../Auth/contexts/auth.context";
+const colors = [Colors.primary_500, "#12B76A", "#9A4CFF", "#F1733D"];
 const ProfileScreen = () => {
-   const { user } = useAuthContext();
-   const { control, handleSubmit } = useForm({ defaultValues: { username: user.username, email: user.email } });
-   const navigation = useNavigation();
-   const handleLogout = () => {
-      removeToken();
-      navigation.replace("Auth", { screen: "Login" });
-   };
-   console.log(user.email);
-   return (
-      <View style={styles.container}>
-         <TextField
-            label="Nombre de usuario"
-            name="username"
-            control={control}
-            rules={{
-               required: "El nombre de usuario es requerido",
-               minLength: {
-                  value: 3,
-                  message: "El nombre de usuario debe tener al menos 3 caracteres",
-               },
-               maxLength: {
-                  value: 80,
-                  message: "El nombre de usuario no debe exceder los 80 caracteres",
-               },
-            }}
-            placeholder="Nombre de usuario"
-            leftIcon={<Icon name="user-fill" size="20" color={Colors.gray_300} />}
-         />
-
-         <TextField
-            label="Email"
-            name="email"
-            control={control}
-            rules={{
-               required: "El nombre de usuario es requerido",
-               minLength: {
-                  value: 3,
-                  message: "El nombre de usuario debe tener al menos 3 caracteres",
-               },
-               maxLength: {
-                  value: 80,
-                  message: "El nombre de usuario no debe exceder los 80 caracteres",
-               },
-            }}
-            placeholder="MAIL"
-            leftIcon={<Icon name="mail-fill" size="20" color={Colors.gray_300} />}
-         />
-         <Button text="Logout" onPress={handleLogout} variant="primary" size="medium" />
+  const { user } = useAuthContext();
+  const navigation = useNavigation();
+  const handleLogout = () => {
+    removeToken();
+    navigation.replace("Auth", { screen: "Login" });
+  };
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.avatarContainer}>
+        <View style={styles.avatar}>
+          <Text style={styles.avatarInitial}>{user.username.slice(0, 1)}</Text>
+        </View>
+        <Text style={styles.usernameText}>{user.username}</Text>
       </View>
-   );
+      <View style={{ flex: 1, width: "100%", gap: 12, marginVertical: 24 }}>
+        <View style={{ flexDirection: "row", flex: 1, gap: 12 }}>
+          <View style={styles.statContainer}></View>
+          <View style={styles.statContainer}></View>
+        </View>
+        <View style={{ flexDirection: "row", flex: 1, gap: 12 }}>
+          <View style={styles.statContainer}></View>
+          <View style={styles.statContainer}></View>
+        </View>
+        <View
+          style={{
+            flexDirection: "column",
+            flex: 1,
+            gap: 12,
+            backgroundColor: "#fff",
+            padding: 12,
+            borderRadius: 12,
+            borderColor: Colors.gray_50,
+            borderWidth: 2,
+            marginTop: 12,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              flex: 1,
+              gap: 12,
+              padding: 12,
+            }}
+          >
+            <UserCirlceAdd
+              size={56}
+              color={Colors.primary_500}
+              variant="Bold"
+            />
+            <View style={{ flex: 1, gap: 4 }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontFamily: "Sora-SemiBold",
+                  color: Colors.primary_600,
+                }}
+              >
+                Una semana de Finex Pro
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  fontFamily: "Sora-Regular",
+                  flex: 1,
+                  color: Colors.gray_400,
+                  lineHeight: 22,
+                }}
+              >
+                Comparte la app con tus amigos y obbten hasta 7 dias gratis de
+                Finex Pro por cada registro
+              </Text>
+            </View>
+          </View>
+          <Button text="Invitar amigos" variant="secondary" size="medium" />
+        </View>
+      </View>
+      <Button
+        text="Cerrar sesión"
+        onPress={handleLogout}
+        variant="primary"
+        size="medium"
+        style={{ alignSelf: "flex-start", marginTop: 24, marginBottom: 76 }}
+      />
+    </ScrollView>
+  );
 };
 
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      padding: 24,
-      justifyContent: "center",
-   },
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  avatarContainer: {
+    flex: 1,
+    gap: 12,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    alignSelf: "center",
+    marginTop: 24,
+    backgroundColor: "#9A4CFF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  avatarInitial: {
+    fontSize: 24,
+    fontFamily: "Sora-Bold",
+    textTransform: "uppercase",
+    color: "#fff",
+  },
+  usernameText: {
+    fontSize: 24,
+    fontFamily: "Sora-SemiBold",
+    textTransform: "capitalize",
+    alignSelf: "center",
+    color: Colors.gray_600,
+  },
+  statContainer: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: Colors.gray_50,
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    padding: 24,
+    height: 164,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- src/features/navigation/BottomNav.navigator.js
+++ src/features/navigation/BottomNav.navigator.js
@@ -1,126 +1,174 @@
 // import React from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Home, Weight, Signpost, Notification, UserOctagon } from "iconsax-react-native";
+import {
+  Home,
+  Weight,
+  Signpost,
+  Notification,
+  UserOctagon,
+} from "iconsax-react-native";
 
 import { SemanticColors, Colors } from "@utils/Theme";
 import * as Screens from "../index";
-import { getFocusedRouteNameFromRoute, useNavigation } from "@react-navigation/native";
+import {
+  getFocusedRouteNameFromRoute,
+  useNavigation,
+} from "@react-navigation/native";
 import WorldStackNavigator from "../World/navigation/WorldStack.navigator";
 const BottomNavStack = createBottomTabNavigator();
 
 export const BottomNavStackNavigator = () => {
-   const navigation = useNavigation();
-   return (
-      <BottomNavStack.Navigator
-         screenOptions={{
-            headerShown: false,
-            tabBarShowLabel: false,
-            tabBarStyle: { height: 72, elevation: 0, shadowOpacity: 0, borderTopColor: Colors.gray_50, borderTopWidth: 6 },
-         }}>
-         <BottomNavStack.Screen
-            name="Lessons"
-            component={WorldStackNavigator}
-            listeners={() => ({
-               tabPress: (e) => {
-                  e.preventDefault();
+  const navigation = useNavigation();
+  return (
+    <BottomNavStack.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 72,
+          elevation: 0,
+          shadowOpacity: 0,
+          borderTopColor: Colors.gray_50,
+          borderTopWidth: 6,
+        },
+      }}
+    >
+      <BottomNavStack.Screen
+        name="Lessons"
+        component={WorldStackNavigator}
+        listeners={() => ({
+          tabPress: (e) => {
+            e.preventDefault();
 
-                  navigation.navigate("Lessons", { screen: "World" });
-               },
-            })}
-            options={({ route }) => ({
-               tabBarIcon: ({ focused }) => (
-                  <Home
-                     variant="Bold"
-                     size={28}
-                     color={focused ? SemanticColors.bg.primary_active : SemanticColors.elevation.secondary_normal}
-                  />
-               ),
-               headerShown: false,
-               tabBarStyle: ((route) => {
-                  const routeName = getFocusedRouteNameFromRoute(route) ?? "";
-                  if (routeName === "Exercise" || routeName === "Congrats" || routeName === "TheoryScreen") {
-                     return { display: "none" };
-                  }
+            navigation.navigate("Lessons", { screen: "World" });
+          },
+        })}
+        options={({ route }) => ({
+          tabBarIcon: ({ focused }) => (
+            <Home
+              variant="Bold"
+              size={28}
+              color={
+                focused
+                  ? SemanticColors.bg.primary_active
+                  : SemanticColors.elevation.secondary_normal
+              }
+            />
+          ),
+          headerShown: false,
+          tabBarStyle: ((route) => {
+            const routeName = getFocusedRouteNameFromRoute(route) ?? "";
+            if (
+              routeName === "Exercise" ||
+              routeName === "Congrats" ||
+              routeName === "TheoryScreen"
+            ) {
+              return { display: "none" };
+            }
 
-                  return { height: 72, elevation: 0, shadowOpacity: 0, borderTopColor: Colors.gray_50, borderTopWidth: 6 };
-               })(route),
-            })}
-            initialParams={{ headerShown: false, headerTitle: "" }}
-         />
+            return {
+              height: 72,
+              elevation: 0,
+              shadowOpacity: 0,
+              borderTopColor: Colors.gray_50,
+              borderTopWidth: 6,
+            };
+          })(route),
+        })}
+        initialParams={{ headerShown: false, headerTitle: "" }}
+      />
 
-         <BottomNavStack.Screen
-            name="Reviews"
-            options={{
-               tabBarIcon: ({ focused }) => (
-                  <Weight
-                     size={28}
-                     variant="Bold"
-                     color={focused ? SemanticColors.bg.primary_active : SemanticColors.elevation.secondary_normal}
-                  />
-               ),
-            }}
-            component={Screens.ReviewsScreen}
-         />
+      <BottomNavStack.Screen
+        name="Reviews"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Weight
+              size={28}
+              variant="Bold"
+              color={
+                focused
+                  ? SemanticColors.bg.primary_active
+                  : SemanticColors.elevation.secondary_normal
+              }
+            />
+          ),
+        }}
+        component={Screens.ReviewsScreen}
+      />
 
-         <BottomNavStack.Screen
-            name="Levels"
-            options={{
-               headerShown: true,
-               headerTitle: "Explora los distintos niveles",
-               headerTitleStyle: {
-                  color: Colors.gray_400,
-                  fontFamily: "Sora-SemiBold",
-                  fontSize: 18,
-               },
-               tabBarItemStyle: {
-                  transform: [{ rotate: "45deg" }],
-                  bottom: 44,
-                  borderWidth: 6,
-                  height: 80,
+      <BottomNavStack.Screen
+        name="Levels"
+        options={{
+          headerShown: true,
+          headerTitle: "Explora los distintos niveles",
+          headerTitleStyle: {
+            color: Colors.gray_400,
+            fontFamily: "Sora-SemiBold",
+            fontSize: 18,
+          },
+          tabBarItemStyle: {
+            transform: [{ rotate: "45deg" }],
+            bottom: 44,
+            borderWidth: 6,
+            height: 80,
 
-                  backgroundColor: "#fff",
-                  borderColor: Colors.gray_50,
-                  borderRadius: 12,
-               },
-               tabBarIconStyle: { transform: [{ rotate: "-45deg" }] },
-               tabBarBadgeStyle: { backgroundColor: "red" },
-               tabBarIcon: ({ focused }) => (
-                  <Signpost
-                     size={28}
-                     variant="Bold"
-                     color={focused ? SemanticColors.bg.primary_active : SemanticColors.elevation.secondary_normal}
-                  />
-               ),
-            }}
-            component={Screens.LevelsScreen}
-         />
-         <BottomNavStack.Screen
-            name="Notifications"
-            options={{
-               tabBarIcon: ({ focused }) => (
-                  <Notification
-                     size={28}
-                     variant="Bold"
-                     color={focused ? SemanticColors.bg.primary_active : SemanticColors.elevation.secondary_normal}
-                  />
-               ),
-            }}
-            component={Screens.NotificationsScreen}
-         />
-         <BottomNavStack.Screen
-            name="Profile"
-            options={{
-               tabBarIcon: ({ focused }) => (
-                  <UserOctagon
-                     variant="Bold"
-                     size={28}
-                     color={focused ? SemanticColors.bg.primary_active : SemanticColors.elevation.secondary_normal}
-                  />
-               ),
-            }}
-            component={Screens.ProfileScreen}
-         />
-      </BottomNavStack.Navigator>
-   );
+            backgroundColor: "#fff",
+            borderColor: Colors.gray_50,
+            borderRadius: 12,
+          },
+          tabBarIconStyle: { transform: [{ rotate: "-45deg" }] },
+          tabBarBadgeStyle: { backgroundColor: "red" },
+          tabBarIcon: ({ focused }) => (
+            <Signpost
+              size={28}
+              variant="Bold"
+              color={
+                focused
+                  ? SemanticColors.bg.primary_active
+                  : SemanticColors.elevation.secondary_normal
+              }
+            />
+          ),
+        }}
+        component={Screens.LevelsScreen}
+      />
+      <BottomNavStack.Screen
+        name="Notifications"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Notification
+              size={28}
+              variant="Bold"
+              color={
+                focused
+                  ? SemanticColors.bg.primary_active
+                  : SemanticColors.elevation.secondary_normal
+              }
+            />
+          ),
+        }}
+        component={Screens.NotificationsScreen}
+      />
+      <BottomNavStack.Screen
+        name="Profile"
+        options={{
+          headerShown: true,
+          headerTitle: "Perfil",
+          tabBarIcon: ({ focused }) => (
+            <UserOctagon
+              variant="Bold"
+              size={28}
+              color={
+                focused
+                  ? SemanticColors.bg.primary_active
+                  : SemanticColors.elevation.secondary_normal
+              }
+            />
+          ),
+        }}
+        component={Screens.ProfileScreen}
+      />
+    </BottomNavStack.Navigator>
+  );
 };
